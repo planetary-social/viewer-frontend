6233f9d4045fba468fc905407bfdeec16939c9ef
Chardot <sebastianheit@gmail.com>
2021-12-16 17:52:45 +0100

    Adds static HTML structure for posts

--- src/view/feed/index.js
+++ src/view/feed/index.js
@@ -21,6 +21,18 @@ function Feed (props) {
             <ul>
                 ${(props.content.data || []).map((post => {
                     return html`<li class="post">
+                        <header class="post_head">
+                            <div class="post_signature>
+                                <a href="#" class="post_author has_stories">
+                                    <img src="" class="post_author_avatar">
+                                </a>
+                                <div class="post_meta">
+                                    <a href="#" class="post_author_name pro_user">Pavel</a>
+                                    <span class="post_timestamp">Monday at 6:32pm</span>
+                                </div>
+                            </div>
+                            <button class="post_options">...</button>
+                        </header>
                         <${Markdown} markdown=${
                             remark()
                                 .use(linkifyHashtags)
@@ -32,6 +44,28 @@ function Feed (props) {
                                 .processSync(post.value.content.text).contents
                             }
                         />
+                        <div class="post_reactions">
+                            <div class="post_comments_by"></div>
+                            <div class="post_actions">
+                                <ul class="action_buttons">
+                                    <li class="action_button"><img src="" class="action_button_image"></li>
+                                    <li class="action_button"><img src="" class="action_button_image"></li>
+                                    <li class="action_button"><img src="" class="action_button_image"></li>
+                                </ul>
+                            </div>
+                            <ul class="post_comments">
+                                <li class="post_comment">
+                                    <header class="comment_author">
+                                        <a href="#" class="post_author_name pro_user">Maven</a>
+                                    </header>
+                                    <main class="comment_body">Hey! That's me by the DJ booth :P Can't believe we had such a fun night dancing our minds to the best industrial techno in Europe 🎆</main>
+                                    <footer class="comment_timestamp">Tuesday at 5:16pm</footer>
+                                </li>
+                            </ul>
+                            <a href="#" class="comment_prompt">
+                                <span class="comment_signup">Sign up</span>&nbsp;to leave a comment</span>
+                            </a>
+                        </div>
                     </li>`
                 }))}
             </ul>
