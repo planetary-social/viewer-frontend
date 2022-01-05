3cef0210339ade13318b9d78f3d2810e0a4bf33d
nichoth <nichoth@gmail.com>
2022-01-04 16:22:43 -0800

    hide md block if theres no text

--- src/view/feed/index.js
+++ src/view/feed/index.js
@@ -67,17 +67,20 @@ function Feed (props) {
                             null
                         }
 
-                        <${Markdown} markdown=${
-                            remark()
-                                .use(linkifyHashtags)
-                                .use(cidToUrl(blobId => {
-                                    return PUB_URL + '/blob/' +
-                                        encodeURIComponent(blobId)
-                                }))
-                                .use(remarkParse, { commonmark: true })
-                                .processSync(post.value.content.text).contents
-                            }
-                        />
+                        ${post.value.content.text ?
+                            html`<${Markdown} markdown=${
+                                remark()
+                                    .use(linkifyHashtags)
+                                    .use(cidToUrl(blobId => {
+                                        return PUB_URL + '/blob/' +
+                                            encodeURIComponent(blobId)
+                                    }))
+                                    .use(remarkParse, { commonmark: true })
+                                    .processSync(post.value.content.text).contents
+                                }
+                            />` :
+                            null
+                        }
 
                         <footer class="post_reactions">
                             <div class="post_actions">
