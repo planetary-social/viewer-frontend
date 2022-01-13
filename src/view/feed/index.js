8b4837d231f07c8e91853885efcc918e0265e01c
nichoth <nichoth@gmail.com>
2022-01-13 10:37:11 -0800

    post merge styles

--- src/view/feed/index.js
+++ src/view/feed/index.js
@@ -10,8 +10,6 @@ var HeadPart = require('../head-part')
 var MsgList = require('../msg-list')
 var Sidebar = require('../sidebar')
 
-console.log('envvvvv', process.env.NODE_ENV)
-
 function Feed (props) {
     if (!props.feed.data) return null
 
@@ -20,86 +18,7 @@ function Feed (props) {
         <${FeedHeader} ...${props} />
 
         <div class="feed-wrapper">
-            <ul class="feed feed-content">
-                ${(props.feed.data || []).map(_post => {
-
-                    // TODO -- handle threads
-                    var post = isThread(_post) ? _post[0] : _post
-                    var { mentions } = post.value.content
-                    var hasImages = !!((mentions || []).filter(m => {
-                        return ref.isBlob(m.link)
-                    })[0])
-
-                    return html`<li class="post ${isThread(_post) ? 'is-thread' : ''}">
-                        <header class="post_head">
-                            <div class="post_signature">
-                                <a href="#" class="post_author has_stories">
-                                    <${MockAvatar} />
-                                </a>
-                                <div class="post_meta">
-                                    <a href="#" class="post_author_name pro_user">Pavel</a>
-                                    <span class="post_timestamp">Monday at 6:32pm</span>
-                                </div>
-                            </div>
-                            <button class="post_options"></button>
-                        </header>
-
-                        ${mentions && mentions[0] && hasImages ?
-                            html`<div class="image-carousel">
-                                ${mentions.map(blob => {
-                                    return ref.isBlob(blob.link) ?
-                                        html`<img src=${PUB_URL + '/blob/' +
-                                            encodeURIComponent(blob.link)}
-                                        />` :
-                                        null
-                                })}
-                            </div>` :
-                            null
-                        }
-
-                        ${post.value.content.text ?
-                            html`<${Markdown} markdown=${
-                                remark()
-                                    .use(linkifyHashtags)
-                                    .use(cidToUrl(blobId => {
-                                        return PUB_URL + '/blob/' +
-                                            encodeURIComponent(blobId)
-                                    }))
-                                    .use(remarkParse, { commonmark: true })
-                                    .processSync(post.value.content.text).contents
-                                }
-                            />` :
-                            null
-                        }
-
-                        <footer class="post_reactions">
-                            <div class="post_actions">
-								<a href="#" class="view-replies_link">View all replies</a>
-                                <ul class="action_buttons">
-									<li class="action_button action_button--share"><a href="#">Share</a></li>
-                                </ul>
-                            </div>
-
-                            <ul class="post_comments">
-                                <li class="post_comment">
-                                    <header class="comment_author">
-                                        <a href="#" class="comment_author_name pro_user">Maven</a>
-                                    </header>
-                                    <main class="comment_body">
-										<p class="comment_text">Hey! That's me by the <a href="#" class="text_link">DJ booth</a> :P Can't believe we had such a fun night dancing our minds to the best industrial techno in Europe 🎆</p>
-									</main>
-                                    <footer class="comment_timestamp">Tuesday at 5:16pm</footer>
-                                </li>
-                            </ul>
-
-                            <a href="#" class="comment_prompt">
-                                <span class="comment_signup text_link">Sign up</span> to leave a comment
-                            </a>
-                        </footer>
-                        <a href="#" class="replies-stack_link"></a>
-                    </li>`
-                })}
-            </ul>
+            <${MsgList} msgs=${props.feed.data} />
             <${Sidebar} ...${props} />
         </div>
     `
@@ -110,6 +29,8 @@ module.exports = Feed
 
 function FeedHeader (props) {
     var { username } = props.feed
+
+    console.log('props', props)
 
     var profile = _.find(props.profiles, { username: props.username })
     if (!profile) return null
@@ -147,30 +68,6 @@ function FeedHeader (props) {
                     </dt>
                 </div>
             </dl>
-        </div>
-    </div>`
-}
-
-function Sidebar (props) {
-    var profile = _.find(props.profiles, { username: props.username })
-
-    return html`<div class="feed-sidebar">
-        <div class="join-today">
-            <h3>Join Planetary today!</h3>
-            <p>Planetary is a decentralized network for people who want to come together and connect even when the internet goes out.</p>
-            <p>It's an app that doesn't keep your data in the cloud.</p>
-
-            <button class="cool-btn">Create your account</button>
-        </div>
-        <div class="whats-hot">
-            <h3>What's hot on Planetary</h3>
-            <ul>
-                <li><a href="#">#CyberPunkRevolution <span class="counter-highlight">(1768)</span></a></li>
-                <li><a href="#">#BlackLivesMatter <span class="counter-highlight">(745)</span></a></li>
-                <li><a href="#">#FreeBritneyNow <span class="counter-highlight">(692)</span></a></li>
-                <li><a href="#">#LaserEyesTill100K <span class="counter-highlight">(581)</span></a></li>
-                <li><a href="#">#KeepHongKongFree <span class="counter-highlight">(437)</span></a></li>
-            </ul>
         </div>
     </div>`
 }
