7c08d26c875f7b8aeee010826f84c3cda922c190
nichoth <nichoth@gmail.com>
2022-01-27 10:13:26 -0800

    accessible icons

--- src/view/post-menu.js
+++ src/view/post-menu.js
@@ -5,26 +5,28 @@ function PostMenu ({ msg, onCloseModal }) {
 
     return html`<div class="modal-options">
         <div class="modal-head">
-            <button class="icon-btn" onclick=${onCloseModal}>
-                <i class="far fa-window-close"></i>
+            <button class="icon-btn" onclick=${onCloseModal}
+                aria-label="Close window"
+            >
+                <i aria-hidden="true" class="far fa-window-close"></i>
             </button>
         </div>
         <ul>
             <li>
-                <button class="opt-btn">
-                    <i class="fas fa-percent"></i>
+                <button class="opt-btn" aria-label="Copy message ID">
+                    <i aria-hidden="true" class="fas fa-percent"></i>
                     Copy message ID
                 </button>
             </li>
             <li>
-                <button class="opt-btn">
-                    <i class="fas fa-book"></i>
+                <button class="opt-btn" aria-label="Copy message text">
+                    <i aria-hidden="true" class="fas fa-book"></i>
                     Copy message text
                 </button>
             </li>
             <li>
-                <button class="opt-btn">
-                    <i class="fas fa-link"></i>
+                <button class="opt-btn" aria-label="Copy link to message">
+                    <i aria-hidden="true" class="fas fa-link"></i>
                     Copy link to message
                 </button>
             </li>
