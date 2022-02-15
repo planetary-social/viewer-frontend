6a32aa9354af35bcc003a097916bb92864426eb6
nichoth <nichoth@gmail.com>
2022-02-15 13:15:50 -0800

    better style

--- src/view/single-message.js
+++ src/view/single-message.js
@@ -3,15 +3,20 @@ const _ = {
     find: require('lodash.find')
 }
 const Post = require('./post')
+var HeadPart = require('./head-part')
 
 function SingleMessage (props) {
     console.log('props in single message', props)
     const msgs = (props.message || {}).msgs
     const msg = _.find(msgs, { key: (props.message || {}).id })
 
-    return html`<div class="single-message">
-        <${Post} post=${msg} />
-    </div>`
+    return html`
+        <${HeadPart} />
+
+        <ul class="single-message">
+            <${Post} post=${msg} />
+        </ul>
+    `
 }
 
 module.exports = SingleMessage
