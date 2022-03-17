e207699edf4c6e854b0fb1455240c3f81f57b578
rabble <evan@protest.net>
2022-03-17 17:48:15 +1300

    changing to new render.com server

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,5 +1,5 @@
 module.exports = {
     PUB_URL: (process.env.NOD_ENV === 'test' ?
         'http://0.0.0.0:8888' :
-        'https://pub2.onrender.com')
+        'https://planetary-link-pub.onrender.com')
 }
